cd65b2b73d33cfa74e169c737e11b5d9a3698441
Carlos Daniel <carlos.daniel@ostenmoove.com.br>
2024-12-03 12:00:11 -0300

    docs: add button group component in storybook

---
+++ packages/docs/stories/button-group.stories.tsx
@@ -0,0 +1,82 @@
+import React from "react";
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { ButtonGroup } from "@osten-moove-ui/button-group";
+import { Button } from "@osten-moove-ui/button";
+
+export default {
+  title: "Inputs/Button Group",
+  component: ButtonGroup,
+  tags: ["autodocs"],
+  argTypes: {
+    variant: {
+      type: "string",
+      control: {
+        type: "select",
+      },
+      options: ["text", "contained", "outlined"],
+    },
+    orientation: {
+      control: "select",
+      options: ["horizontal", "vertical"],
+    },
+    disabled: {
+      type: "boolean",
+      control: {
+        type: "boolean",
+      },
+    },
+    disableElevation: {
+      type: "boolean",
+      control: {
+        type: "boolean",
+      },
+    },
+    color: {
+      type: "string",
+      control: {
+        type: "select",
+      },
+      options: [
+        "error",
+        "info",
+        "inherit",
+        "primary",
+        "secondary",
+        "success",
+        "warning",
+      ],
+    },
+    size: {
+      type: "string",
+      control: "select",
+      options: ["small", "medium", "large"],
+    },
+    fullWidth: {
+      control: "boolean",
+    },
+    numberOfButtons: {
+      control: {
+        type: "number",
+        max: 10,
+      },
+    },
+    sx: {
+      control: "object",
+      description: "Style overrides using Material UI sx prop.",
+    },
+  },
+  render: ({ numberOfButtons = 3, ...args }) => {
+    return (
+      <ButtonGroup {...args}>
+        {Array.from({ length: numberOfButtons }).map((_, number) => {
+          return <Button>Botão {number + 1}</Button>;
+        })}
+      </ButtonGroup>
+    );
+  },
+} as Meta<typeof ButtonGroup>;
+
+export const Default: StoryObj<typeof ButtonGroup> = {
+  args: {},
+};
